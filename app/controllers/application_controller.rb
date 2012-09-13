508cf0c486d9b49ab8e1738836fb337aee1c11f8
Shannon McCoy <sglenmccoy@gmail.com>
2012-09-13 15:08:02 -0600

    Finished Static Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   def index
     @users = user.all
+  end
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+describe "Home Page" do
+    it "should have the content 'Message Boards'" do
+      visit '/static_pages/home'
+      page.should have_content('Message Boards')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Message Boards | Home")
+    end
+  end
+  
+describe "Help Page" do
+  it "should have the content 'Help" do
+    visit '/static_pages/help'
+    page.should have_content ('Help')
+   end
+   
+    it "should have the right title" do
+       visit '/static_pages/help'
+       page.should have_selector('title',
+                         :text => "Message Boards | Help")
+   end
+ end
+ 
+ describe "About page" do
+     it "should have the content 'About'" do
+       visit '/static_pages/about'
+       page.should have_content('About')
+     end
+     
+      it "should have the right title" do
+         visit '/static_pages/about'
+         page.should have_selector('title',
+                           :text => "Message Boards | About")
+    end
+  end
+end
