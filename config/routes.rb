64a66b43fbb01afecde07701d05021acd584df6b
Shannon McCoy <sglenmccoy@gmail.com>
2012-09-18 08:46:31 -0600

    Added Users & Passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Message::Application.routes.draw do
-
-  get "users/new"
-
-  match '/signup', to: 'users#new'
   
   match '/', to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup', to: 'users#new'
 
   resources :microposts
 
